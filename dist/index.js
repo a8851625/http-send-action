27d03df4009bc518d66756f7e5c0a645e1073d3c
moon.huang <moon.huang@moonhuangdeMacBook-Pro.local>
2024-12-27 11:45:37 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -35939,7 +35939,7 @@ async function run() {
     }
 
     // 设置辅助解析结果的输出
-    core.setOutput('parsedResult', JSON.stringify(parsedResult));
+    core.setOutput('parsedResult', parsedResult);
 
   } catch (error) {
     core.setFailed(`Request failed: ${error.message}`);
